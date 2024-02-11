d0b4c0419cecf2f6c0ea37e88c660a294c4ebd8a
Lucimario <lucimario.ramos@outlook.com>
2024-02-10 23:13:01 -0300

    Arquivo pronto

--- Calculadoras_de_partidas_hankeadas.js
+++ Calculadoras_de_partidas_hankeadas.js
@@ -1,60 +1,72 @@
 //2️⃣ Calculadora de partidas Rankeadas
 
 function rank (vitorias, derrotas){
-let saldoDeVitoria = vitorias - derrotas
-function heroi()
-}
-
-function heroi (saldoDeVitoria){
-
-    let nivel
-
-    switch (saldoDeVitoria) {
-        case 1:
-            saldoDeVitoria <= 10
-            nivel = "Ferro"
-            break;
-    
-        case 2:
-            saldoDeVitoria > 10 && saldoDeVitoria <= 20
-            nivel = "Bronze"
-            break;
-
-        case 3:
-            saldoDeVitoria > 20 && saldoDeVitoria <= 50
-            nivel = "Prata"
-            break
-            
-        case 4:
-            saldoDeVitoria > 50 && saldoDeVitoria <= 80
-            nivel = "Ouro"
-            break
-            
-        case 5:
-            saldoDeVitoria > 80 && saldoDeVitoria <= 90
-            nivel = "Diamnte"
-            break
-       
-        case 6:
-            saldoDeVitoria > 90 && saldoDeVitoria <= 100
-            nivel = "Lendário"
-            break
-
-        case 7:
-            saldoDeVitoria > 101
-            nivel = "Inortal"
-            break
-
-        
+    let saldoDeVitoria = vitorias - derrotas
+    heroi (saldoDeVitoria)
     }
-
-    function informacao()
-}
-
-function informacao (saldoDeVitoria, nivel){
-
-    console.log("O Heroi tem saldo de " + saldoDeVitoria "está no vivel " + nivel)
-
-}
-
-function rank(320, 217)
+    let nivel 
+    function heroi (saldoDeVitoria){
+    
+        
+    
+        if(saldoDeVitoria <= 10){
+                nivel = "Ferro"
+            informacao (saldoDeVitoria, nivel)
+    
+        }
+                
+        
+            else if (saldoDeVitoria > 10 && saldoDeVitoria <= 20){
+                nivel = "Bronze"
+                informacao (saldoDeVitoria, nivel)
+    
+            }
+    
+            else if (saldoDeVitoria > 20 && saldoDeVitoria <= 50){
+                nivel = "Prata"
+                informacao (saldoDeVitoria, nivel)
+    
+            }
+                
+                
+            else if (saldoDeVitoria > 50 && saldoDeVitoria <= 80){
+                nivel = "Ouro"
+                informacao (saldoDeVitoria, nivel)
+    
+            }
+                
+                
+            else if (saldoDeVitoria > 80 && saldoDeVitoria <= 90){
+                nivel = "Diamnte"
+                informacao (saldoDeVitoria, nivel)
+    
+            }
+                
+           
+            else if (saldoDeVitoria > 90 && saldoDeVitoria <= 100){
+                nivel = "Lendário"
+                informacao (saldoDeVitoria, nivel)
+    
+            }
+                
+    
+            else {
+                nivel = "Imortal"
+                informacao (saldoDeVitoria, nivel)
+    
+            }
+                
+    
+            
+        }
+    
+        
+    
+    function informacao (saldoDeVitoria, nivel){
+    
+        console.log("O Heroi tem saldo de " + saldoDeVitoria + " esta no nivel " + nivel)
+    
+    }
+    
+    rank(320, 217)
+    
